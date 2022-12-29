b60eeb2769d9b3ba424e8617c8a6056839861539
sksabbirhossain <sksabbirhossain4@gmail.com>
2022-12-29 11:28:55 +0600

    created a function to delete task

--- controller/taskController.js
+++ controller/taskController.js
@@ -1,4 +1,22 @@
+const { ObjectId } = require("mongodb");
 const { taskCollection } = require("../database/database");
+
+//get task by user id
+const myTask = async (req, res) => {
+  try {
+    const { userid } = req.query;
+    const myTasks = await taskCollection.find({ userId: userid }).toArray();
+    res.send({
+      success: true,
+      data: myTasks,
+    });
+  } catch (err) {
+    res.send({
+      success: false,
+      error: err.message,
+    });
+  }
+};
 
 //add task
 const addtask = async (req, res) => {
@@ -16,6 +34,32 @@ const addtask = async (req, res) => {
   }
 };
 
+//delete a task
+const deleteTask = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const result = await taskCollection.deleteOne({ _id: ObjectId(id) });
+    if (result.deletedCount) {
+      res.send({
+        success: true,
+        message: "User Delete successfull",
+      });
+    } else {
+      res.send({
+        success: false,
+        message: "user not deleted",
+      });
+    }
+  } catch (error) {
+    res.send({
+      success: false,
+      message: "something worng",
+    });
+  }
+};
+
 module.exports = {
   addtask,
+  myTask,
+  deleteTask,
 };
--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { addtask } = require("../controller/taskController");
+const { addtask, myTask, deleteTask } = require("../controller/taskController");
 
 const router = express.Router();
 
+router.get("/my-task", myTask);
 router.post("/add-task", addtask);
+router.delete("/my-task/:id", deleteTask)
 
 module.exports = router;
