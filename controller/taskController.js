28bbee108e4f16da3d49968e1394287b8fc78b91
sksabbirhossain <sksabbirhossain4@gmail.com>
2022-12-29 12:51:13 +0600

    update task by id

--- controller/taskController.js
+++ controller/taskController.js
@@ -5,7 +5,9 @@ const { taskCollection } = require("../database/database");
 const myTask = async (req, res) => {
   try {
     const { userid } = req.query;
-    const myTasks = await taskCollection.find({ userId: userid }).toArray();
+    const myTasks = await taskCollection
+      .find({ userId: userid, status: false })
+      .toArray();
     res.send({
       success: true,
       data: myTasks,
@@ -14,6 +16,50 @@ const myTask = async (req, res) => {
     res.send({
       success: false,
       error: err.message,
+    });
+  }
+};
+
+//get task by task id for update
+const getUpdateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await taskCollection.find({ _id: ObjectId(id) }).toArray();
+    res.send({
+      success: true,
+      data: task,
+    });
+  } catch (err) {
+    res.send({
+      success: false,
+      error: err.message,
+    });
+  }
+};
+
+//get task by task id for update
+const updateTask = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const task = await taskCollection.updateOne(
+      { _id: ObjectId(id) },
+      { $set: req.body }
+    );
+    if (task.matchedCount) {
+      res.send({
+        success: true,
+        message: "update successfull",
+      });
+    } else {
+      res.send({
+        success: false,
+        message: "something went worng!",
+      });
+    }
+  } catch {
+    res.send({
+      success: false,
+      message: "something went worng!",
     });
   }
 };
@@ -62,4 +108,6 @@ module.exports = {
   addtask,
   myTask,
   deleteTask,
+  getUpdateTask,
+  updateTask,
 };
--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { addtask, myTask, deleteTask } = require("../controller/taskController");
+const { addtask, myTask, deleteTask, getUpdateTask, updateTask } = require("../controller/taskController");
 
 const router = express.Router();
 
 router.get("/my-task", myTask);
 router.post("/add-task", addtask);
+router.get("/my-task/update/:id", getUpdateTask)
+router.patch("/my-task/update/:id", updateTask)
 router.delete("/my-task/:id", deleteTask)
 
 module.exports = router;
